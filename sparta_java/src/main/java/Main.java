3128600faa78fcd56c15bb89c83fe3bb9efa6984
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-06-28 13:09:00 +0900

    노트북 테스트

--- sparta_java/src/main/java/Main.java
+++ sparta_java/src/main/java/Main.java
@@ -1,17 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        int[] absolutes = {4,7,12};
-        boolean[] signs = {true, false, true};
-        int answer = 0;
-        for (int i=0; i<absolutes.length; i++){
-            if (signs[i] == true){
-                answer += absolutes[i];
-            }else {
-                answer -= absolutes[i];
-            }
-        }
-        System.out.println(answer);
+
     }
 }
 
